46cd5c219ec1dffd8ce3cb051a10b1b96dfe1892
davidgarcia <dfgarcias.dev@gmail.com>
2021-09-22 19:56:35 -0500

    :sparkles: Initiate project

---
+++ src/test/java/se/daga/RunApplicationTests.java
@@ -0,0 +1,13 @@
+package se.daga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
